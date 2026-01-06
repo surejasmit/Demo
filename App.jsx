7bd4f878bb5399326134436566ee599fd7b3a29c
Talaviya-Sarthak <stalaviya709@gmail.com>
2026-01-06 22:53:30 +0530

    added one line

--- App.jsx
+++ App.jsx
@@ -5,7 +5,9 @@ import ProfileCard from "./components/ProfileCard";
 const users = [
   { id: 1, name: "Smit Sureja", image: "https://via.placeholder.com/300" },
   { id: 2, name: "John Doe", image: "https://images.unsplash.com/photo-1502685104226-ee32379fefbe" },
-  { id: 3, name: "Jane Smith", image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2" }
+  { id: 3, name: "Jane Smith", image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2" },
+  { id: 4, name: "Alex Johnson", image: "https://images.unsplash.com/photo-1502685104226-ee32379fefbe" },
+  
 ];
 
 function App() {
