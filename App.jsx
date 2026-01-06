611ea9a6b2f50a5f0eb10e635f211d4896fd217b
smit-66 <24aiml@charusat.edu.in>
2026-01-06 22:48:16 +0530

    change in App.jsx

--- App.jsx
+++ App.jsx
@@ -5,7 +5,7 @@ import ProfileCard from "./components/ProfileCard";
 const users = [
   { id: 1, name: "Smit Sureja", image: "https://via.placeholder.com/300" },
   { id: 2, name: "John Doe", image: "https://images.unsplash.com/photo-1502685104226-ee32379fefbe" },
-  { id: 3, name: "Jane Smith", image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2" }
+  
 ];
 
 function App() {
